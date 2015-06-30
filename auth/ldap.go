fec36cb1258572537d94fd85475996157f69e7ee
BC Broussard <bc.broussard@flyingcranes.com>
2015-06-30 13:03:10 -0700

    Initial support for ldap - wip

---
+++ auth/ldap.go
@@ -0,0 +1,81 @@
+package auth
+
+import (
+	// "crypto/sha1"
+	// "crypto/tls"
+	// "encoding/base64"
+	// "errors"
+	"fmt"
+	"github.com/bcbroussard/k8s-ldap/ldap"
+	"log"
+	// "net"
+	"net/http"
+	// "strings"
+	"crypto/sha1"
+	"encoding/base64"
+	"net/http"
+	"strings"
+)
+
+type LdapAuth struct {
+	ldapServer string
+	ldapPort   uint
+	username   string
+	insecure   bool
+}
+
+func RequireBasicAuth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("WWW-Authenticate", `Basic realm="kubernetes ldap"`)
+	w.WriteHeader(401)
+	w.Write([]byte("401 Unauthorized\n"))
+}
+
+// Grab credentials from the request
+func ParseCredentials(r *http.Request) (username, password string) {
+
+	username = "admin"    //pair[0]
+	password = "passw0rd" //pair[1]
+
+	return
+}
+
+func (a *LdapAuth) Authenticate(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// var l *Conn
+		// var err error
+
+		// if a.insecure {
+		l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", a.ldapServer, a.ldapPort))
+		// } else {
+		// 	l, err := ldap.DialTLS("tcp", fmt.Sprintf("%s:%d", a.ldapServer, a.ldapPort), nil)
+		// }
+
+		if err != nil {
+			log.Fatalf("ERROR: %s\n", err.Error())
+		}
+		defer l.Close()
+
+		// LDAP Auth
+		l.Debug = true
+
+		user, passwd := ParseCredentials(r)
+
+		err = l.Bind(user, passwd)
+		if err != nil {
+			log.Printf("ERROR: Cannot bind: %s\n", err.Error())
+
+			RequireBasicAuth(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+
+	})
+
+}
+
+func NewLdapAuth(ldapServer string, ldapPort uint, insecure bool) *LdapAuth {
+	return &LdapAuth{ldapServer: ldapServer, ldapPort: ldapPort, insecure: insecure}
+}
---
+++ k8s-ldap.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	// "crypto/tls"
+	// "errors"
+	"flag"
+	// "fmt"
+	"github.com/bcbroussard/k8s-ldap/auth"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+// Move these to vars to flags
+
+var insecure = flag.Bool("ldap-insecure", true, "Disable LDAP SSL/TLS")
+var ldapHost = flag.String("ldap-host", "localhost", "description")
+var ldapPort = flag.Uint("ldap-port", 339, "description")
+
+// Change to consistent format (host/port)
+var apiserver = flag.String("apiserver", "http://localhost:8080", "Address of Kubernetes API server")
+
+func NewSingleHostReverseProxy(url *url.URL) *httputil.ReverseProxy {
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	// Rewrite the host
+	oldDirector := proxy.Director
+	proxy.Director = func(r *http.Request) {
+		oldDirector(r)
+		r.Host = url.Host
+	}
+	return proxy
+}
+
+func main() {
+
+	flag.Parse()
+	// if flag.NArg() != 1 {
+	// 	flag.Usage()
+	// }
+
+	l := auth.NewLdapAuth(*ldapHost, *ldapPort, *insecure)
+
+	target, err := url.Parse(*apiserver)
+	// target, err := url.Parse("http://jsonplaceholder.typicode.com:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	proxy := NewSingleHostReverseProxy(target)
+
+	http.Handle("/", l.Authenticate(proxy))
+
+	log.Fatal(http.ListenAndServe(":4000", nil))
+
+}
